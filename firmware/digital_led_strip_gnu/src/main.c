d840e828dbda53a83fe4aff513aaab34c331da45
Zach Banks <zbanks@mit.edu>
2014-07-23 12:15:34 -0400

    DMX-esque + Bespeckle modes

--- firmware/digital_led_strip_gnu/src/main.c
+++ firmware/digital_led_strip_gnu/src/main.c
@@ -4,84 +4,75 @@
 #include "lux.h"
 //#include "dmx.h"
 
-const char a[]="UUUUUUUUUUUUUU";
-
-uint8_t uart_buffer[10];
-uint8_t uart_idx = 0;
-
 #define STRIP_LENGTH (STRIP_PIXELS)
+#define DEVICE_ADDRESS      (0x00)
+#define DEVICE_ADDRESS_MASK (0x00)
 
+//#define MODE_BESPECKLE
 
-int main(void)
-{
-  static int16_t a;
-  uint8_t btn;
-  int16_t ser;
-  uint8_t k = 0;
-  canpacket_t msg1 = {0x03, 'a', {0x80, 20, 30, 0x00, 0x00, 0x00}};
-  canpacket_t msg_tick = {CMD_TICK, 0, {0, 0, 0, 0, 0, 0}};
-  message(&msg1);
-  message(&msg_tick);
-  
-  init();
-  init_effects_heap();
-  for(int i=0;i<STRIP_LENGTH;i++){
-    strip_data[i]=pack_RGB(5,0,0);
-}
-    strip_refresh();
-    populate_strip(strip_data);
-    strip_refresh();
-  while(1)
-  {
-    luxframe_t* luxframe;
-    int sum = 0;
+#ifdef MODE_BESPECKLE
+#undef MODE_DMX
+#else
+#define MODE_DMX
+#endif
 
+#define MAX(a, b) ((a > b) ? a : b)
+#define MIN(a, b) ((a < b) ? a : b)
 
-    luxframe = lux_usart_poll();
-    set_led(0);
-    if(luxframe){
-        if(luxframe->address == 0x00){
-            //Example
+int main(void) {
+    uint8_t btn;
+
+    #ifdef MODE_BESPECKLE
+        canpacket_t msg1 = {0x03, 'a', {0x80, 20, 30, 0x00, 0x00, 0x00}};
+        canpacket_t msg_tick = {CMD_TICK, 0, {0, 0, 0, 0, 0, 0}};
+        message(&msg1);
+        message(&msg_tick);
+
+        init();
+        init_effects_heap();
+        for(int i=0;i<STRIP_LENGTH;i++){
+            strip_data[i]=pack_RGB(5,0,0);
         }
-        if(luxframe->length >= 8){
-            message(luxframe->data);
-            if(luxframe->data[0] & 0x80){
+        strip_refresh();
+        populate_strip(strip_data);
+        strip_refresh();
+    #endif
+
+    #ifdef MODE_DMX
+        for(int i=0;i<STRIP_LENGTH;i++){
+            strip_data[i]=pack_RGB(0,3,0);
+        }
+        strip_refresh();
+    #endif
+    while(1) {
+        luxframe_t* luxframe;
+
+        luxframe = lux_usart_poll();
+        set_led(0);
+        if(luxframe){
+            if(luxframe->address & DEVICE_ADDRESS_MASK == DEVICE_ADDRESS & DEVICE_ADDRESS_MASK){
+                //Example
+                #ifdef MODE_BESPECKLE
+                if(luxframe->length >= 8){
+                    message(luxframe->data);
+                    if(luxframe->data[0] & 0x80){
+                        set_led(1);
+                        populate_strip(strip_data); 
+                        strip_refresh();
+                    }else{
+                    }
+                }
+                #endif
+
+                #ifdef MODE_DMX
                 set_led(1);
-                populate_strip(strip_data); 
+                memcpy(strip_data, luxframe->data, MIN(STRIP_LENGTH, luxframe->length));
                 strip_refresh();
-            }else{
+                #endif
             }
         }
+        strip_refresh_nowait();
     }
-    /*
-    message(&msg_tick);
-    compose_all(effects, strip_data); 
-    */
-    
-    //btn=get_button();
-    //set_led(btn);
-    //poll_usart();
-    //send_str(a, 10);
-    
-
-    /*
-    if(btn)
-    {
-      for(int i=0;i<STRIP_LENGTH;i++)
-        strip_data[i]=pack_RGB(0,15,5);
-    }
-    else
-    {
-      //for(int i=0;i<STRIP_LENGTH;i++){
-        //strip_data[i]=pack_RGB(5,0,5);
-     // for(long i=1;i < 100000;i++){
-        //strip_data[i]=pack_RGB(0,0,0);
-      //}
-    }
-    //strip_refresh();
-    */
-    strip_refresh_nowait();
-  }
 }
 
 #ifdef  USE_FULL_ASSERT
